0b180f33c935bc02167f0cf88fd18c6869275cff
Zbuddy13 <110738622+Zbuddy13@users.noreply.github.com>
2024-12-11 22:05:22 -0500

    added json functionality

---
+++ run.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def neweggButton():
+    r = requests.get('https://www.newegg.com/p/N82E16814883006')
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    s = soup.find('button', class_='btn')
+
+    print(s.get_text())
+
+def neweggPage():
+    r = requests.get('https://www.newegg.com/p/pl?d=b580')
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    itemNames = soup.find_all('a', class_='item-title')
+    itemPrices = soup.find_all('div', class_='item-button-area')
+    filteredNames = [string for string in itemNames if "B580" in string.get_text()]
+
+    elements = len(filteredNames)
+
+    thisDict = {}
+    for i in range(elements):
+        thisDict[filteredNames[i].get_text()] = itemPrices[i].get_text()
+    
+    thisJson = json.dumps(thisDict)
+
+    print(thisJson)
+
+
+neweggPage()
